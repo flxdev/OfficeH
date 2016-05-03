dd0a0d2772d25da73cc50f6c227c27e4b30ebedf
Developer <Developer>
2016-05-03 15:36:18 +0300

    popup menu

--- bitrix/templates/main/js/common.js
+++ bitrix/templates/main/js/common.js
@@ -133,6 +133,41 @@ $(document).ready(function () {
 		$('.grid-gallery').fancybox({
 			padding: 0
 		});
-	}
+	};
+
+	//menu btn
+
+	(function(){
+		var hDoc = $(document).height(),
+			hWind = $(window).height(),
+			hHeader = $('.header').height(),
+			btn = $('.menu'),
+			mMenu = $('.menu__wrapper'),
+			close = mMenu.find('.close__menu');
+
+		btn.on('click', function(){
+			mMenu.addClass('is-active');
+		});
+
+		mMenu.add(btn).on('click', function(event){
+			event.stopPropagation();
+		});
+
+		$(document).add(close).on('click', function(){
+			mMenu.removeClass('is-active');
+		});
+
+		$(document).scroll(function() {    
+			var scroll = $(this).scrollTop();
+			if (scroll > hHeader) {
+				btn.addClass('is-active');
+			}
+			else{
+				btn.removeClass('is-active');
+				mMenu.removeClass('is-active');
+			}
+		});
+
+	})();
 
 })
